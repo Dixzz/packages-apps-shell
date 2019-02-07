2188cb2709042b76038d48d54e550535b829c9ff
Satyam Sharma <33489151+Dixzz@users.noreply.github.com>
2019-02-07 21:38:36 +0530

    Update SecureSettingSeekBarPreference.java
    Thanks to havoc

--- src/com/pearl/shell/preferences/SecureSettingSeekBarPreference.java
+++ src/com/pearl/shell/preferences/SecureSettingSeekBarPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.havoc.settings.preferences;
+package com.pearl.shell.preferences;
 
 import android.content.Context;
 import android.util.AttributeSet;
